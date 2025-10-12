3a68fbce035220b3c82cc2766b430160de45ec26
Ryan Laleh <33795357+RyN21@users.noreply.github.com>
2025-10-11 21:42:37 -0600

    Create game window

---
+++ main.rb
@@ -0,0 +1,19 @@
+require 'gosu'
+
+class GameWindow < Gosu::Window
+  def initialize
+    super(640, 480)
+    self.caption = "Starter Game"
+  end
+
+  def update
+
+  end
+
+  def draw
+
+  end
+end
+
+window = GameWindow.new
+window.show
